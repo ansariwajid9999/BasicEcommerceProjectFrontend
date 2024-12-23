404e1838384434626aba6b2da4771712e9b3860f
Wajid <ansariwajid9999@gmail.com>
2024-12-23 20:14:36 +0530

    code working

--- react-app/src/ProductList.jsx
+++ react-app/src/ProductList.jsx
@@ -6,12 +6,12 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
-    const res = await axios.get("/api/products");
+    const res = await axios.get("http://localhost:3030/api/products");
     console.log(res.data);
     setProducts(res.data);
   };
   const handleClick = async (id) => {
-    const res = await axios.delete(`/api/products/delete/${id}`);
+    const res = await axios.delete(`http://localhost:3030/api/products/delete/${id}`);
     console.log(res.data);
     if (res.data._id) {
       setProducts(products.filter((p) => p._id !== res.data._id));
